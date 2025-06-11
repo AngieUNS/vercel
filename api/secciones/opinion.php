a95e48bf8578c747ac49d9bbf8fd7f6c74285868
Angie Castillo <109643676+AngieUNS@users.noreply.github.com>
2025-06-11 10:16:38 -0500

    Update opinion.php

--- api/secciones/opinion.php
+++ api/secciones/opinion.php
@@ -1,11 +1,11 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERÉS EN ESTA CATEGORÍA
 *****/
 
 $opinion = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "La justicia y el poder en el Perú (2025-2026)",
+    "autor" => "Omar Cairo",
+    "resumen" => "Omar Cairo reflexiona sobre la crisis política en Perú, destacando la renuncia del primer ministro Gustavo Adrianzén y el asesinato de 13 mineros. A pesar de la creciente impopularidad de la presidenta Dina Boluarte, se mantiene en el poder sin reformas significativas, mientras la corrupción persiste en la élite política.",
 ];
 ?>
