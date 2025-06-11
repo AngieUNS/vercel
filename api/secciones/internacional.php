df33420342474b44a014f842b1f688d1c78d892d
Angie Castillo <109643676+AngieUNS@users.noreply.github.com>
2025-06-11 10:12:54 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -1,11 +1,11 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERÉS EN ESTA CATEGORÍA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Conflicto global aumenta tensiones diplomáticas",
+    "autor" => "Laura Martínez",
+    "resumen" => "Las recientes disputas entre países han elevado la tensión internacional, generando llamados a la calma y negociaciones multilaterales.",
 ];
 ?>
