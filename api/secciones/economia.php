d1c3acda00ff2358da5e0b93ffd5df2048694f0c
Angie Castillo <109643676+AngieUNS@users.noreply.github.com>
2025-06-11 10:17:44 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -5,7 +5,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERÉS EN ESTA CATEGORÍA
 
 $economia = [
     "titulo" => "Banco Mundial proyecta crecimiento económico de 2,5% para Perú en 2025",
-    "autor" => "Redacción El Comercio",
+    "autor" => "Redacción RPP",
     "resumen" => "El Banco Mundial ha proyectado que la economía peruana crecerá un 2,5% en 2025 y 2026, destacando la importancia de las inversiones en el sector minero e infraestructura para respaldar este crecimiento. La moderación en el consumo y la política fiscal también influirán en la expansión económica del país.",
 ];
 ?>
