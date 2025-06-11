584338eb6ce8d6a38da95437d0509abf478b2b85
Angie Castillo <109643676+AngieUNS@users.noreply.github.com>
2025-06-11 10:15:22 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -6,6 +6,6 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERÉS EN ESTA CATEGORÍA
 $nacional = [
     "titulo" => "Consulta popular de revocatoria en cuatro distritos del Perú",
     "autor" => "Redacción El Comercio",
-    "resumen" => "El 8 de junio de 2025 se llevaron a cabo consultas populares de revocatoria en los distritos de Boquerón (Ucayali), Huamalí (Junín), Huayana (Apurímac) y Julcamarca (Huancavelica). Los ciudadanos votaron para decidir si las autoridades municipales elegidas en 2022 continuaban en sus cargos. Esta consulta es un mecanismo de control ciudadano establecido por el Jurado Nacional de Elecciones, y se realiza en el segundo año del mandato de las autoridades sometidas a revocatoria.",
+    "resumen" => "El 8 de junio de 2025 se llevaron a cabo consultas populares de revocatoria en los distritos de Boquerón (Ucayali), Huamalí (Junín), Huayana (Apurímac) y Julcamarca (Huancavelica).",
 ];
 ?>
